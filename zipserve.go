83953be6e65832186883f1338397fdee54598da5
Abhijit Hoskeri <abhijithoskeri@gmail.com>
2016-10-29 21:23:44 -0700

    failing test

--- zipserve.go
+++ zipserve.go
@@ -2,9 +2,38 @@ package zipserve
 
 import "net/http"
 import "archive/zip"
-import "fmt"
+import "os"
 
-type entryMap map[string]*zip.File
+type ZipHttpFile struct {
+	z *zip.File
+	http.File
+}
+
+func NewZipHttpFile(z *zip.File) ZipHttpFile {
+	return ZipHttpFile{z: z}
+}
+
+func (z ZipHttpFile) Read(p []byte) (n int, err error) {
+	return 0, nil
+}
+
+func (z ZipHttpFile) Close() error {
+	return nil
+}
+
+func (z ZipHttpFile) Seek(offset int64, whence int) (ret int64, err error) {
+	return
+}
+
+func (z ZipHttpFile) ReadDir(count int) (entries []os.FileInfo, err error) {
+	return
+}
+
+func (z ZipHttpFile) Stat() (fi os.FileInfo, err error) {
+	return
+}
+
+type entryMap map[string]ZipHttpFile
 
 type ZipServer struct {
 	http.Handler
@@ -23,11 +52,15 @@ func New(f string) (z *ZipServer, err error) {
 	}
 
 	for _, f := range z.zip.File {
-		z.entries[f.Name] = f
+		z.entries[f.Name] = NewZipHttpFile(f)
 	}
 	return
 }
 
+func (z *ZipServer) Open(path string) (f http.File, err error) {
+	return z.entries[path], nil
+}
+
 func (z *ZipServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("zipfile"))
+	return
 }
--- zipserve_test.go
+++ zipserve_test.go
@@ -1,7 +1,21 @@
 package zipserve
 
 import "testing"
+import "net/http/httptest"
+import "bytes"
 
 func TestZipRead(t *testing.T) {
+	z, err := New("testdata/test.jar")
+	if err != nil {
+		t.Fatalf("failed to initialize %s", err)
+	}
 
+	req := httptest.NewRequest("GET", "/a.txt", nil)
+	w := httptest.NewRecorder()
+
+	z.ServeHTTP(w, req)
+	if w.Body != bytes.NewBuffer([]byte("aaaa")) {
+		t.Log(w.Body)
+		t.Fail()
+	}
 }
