c6516f88b938f86ccb732705430f92812bf0eecf
Abhijit Hoskeri <abhijithoskeri@gmail.com>
2017-07-25 21:11:15 -0700

    wip

--- cmd/zipserver/main.go
+++ cmd/zipserver/main.go
@@ -8,15 +8,17 @@ import "github.com/hoskeri/zipserve"
 type zipserver struct {
 	spec     string
 	zipfiles []string
-	entries  []zipserve.ZipFileSystem
+	entries  []*zipserve.ZipFileSystem
 }
 
 func main() {
 	zs := zipserver{}
 	var single string
+	var listenAddr string
 
 	flag.StringVar(&zs.spec, "spec", "", "list of zip file paths")
 	flag.StringVar(&single, "zip", "", "single zip file")
+	flag.StringVar(&listenAddr, "addr", ":8080", "http listen address")
 	flag.Parse()
 
 	if zs.spec == "" && single == "" {
@@ -32,5 +34,11 @@ func main() {
 		log.Printf("serving from spec: %s\n", zs.spec)
 	}
 
-	log.Printf("entries: %s\n", zs.zipfiles)
+	for _, e := range zs.zipfiles {
+		z, err := zipserve.New(e)
+		if err != nil {
+			log.Fatalf("failed to open zip file %s: %s", e, err)
+		}
+		zs.entries = append(zs.entries, z)
+	}
 }
